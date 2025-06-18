e3f8d317538e7de9127830b1b60e727e99a52604
SadiqRaja1 <sortmode314@gmail.com>
2025-06-18 06:27:34 +0530

    Header fully created

--- client/login/src/Header/Header.jsx
+++ client/login/src/Header/Header.jsx
@@ -2,13 +2,13 @@ import {Link} from "react-router-dom"
 
 export default function Header () {
   return(
-    <header className="w-full bg-black">
-      <div>
+    <header className="w-screen h-14 flex justify-between px-16 items-center bg-blue-600 text-white rounded-b-full">
+      <div className="text-3xl">
         NotLogged In
       </div>
-      <navbar>
-        <Link to="/singup">Singup</Link>
-        <Link to="/login">Login</Link>
+      <navbar className="text-2xl">
+        <Link className="hover:text-black" to="/singup">Singup</Link>
+        <Link className="ml-3 hover:text-black" to="/login">Login</Link>
       </navbar>
     </header>
     )
