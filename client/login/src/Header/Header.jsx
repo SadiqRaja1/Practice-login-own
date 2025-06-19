1cdf1718684d48916868bdaf4c50c56afe754806
SadiqRaja1 <sortmode314@gmail.com>
2025-06-19 12:34:14 +0530

    update header for mobile user

--- client/login/src/Header/Header.jsx
+++ client/login/src/Header/Header.jsx
@@ -3,10 +3,10 @@ import {Link} from "react-router-dom"
 export default function Header () {
   return(
     <header className="w-full h-14 flex justify-between px-16 items-center bg-blue-600 text-white rounded-b-full">
-      <div className="text-3xl">
+      <div className="text-xl sm:text-3xl">
         NotLogged In
       </div>
-      <nav className="text-2xl">
+      <nav className="sm:text-2xl">
         <Link className="hover:text-black" to="/signup">Signup</Link>
         <Link className="ml-3 hover:text-black" to="/login">Login</Link>
       </nav>
