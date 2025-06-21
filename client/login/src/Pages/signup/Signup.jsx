cbe231f60b9f72ab9ee516937e37e46ad0340006
SadiqRaja1 <sortmode314@gmail.com>
2025-06-21 05:45:45 +0530

    singup form updated

--- client/login/src/Pages/signup/Signup.jsx
+++ client/login/src/Pages/signup/Signup.jsx
@@ -1,16 +1,31 @@
-
+import { useState } from 'react';
 
 export default function Singup () {
+  
+  const [data, setData] = useState({
+    name:"",
+    email:"",
+    password:""
+  });
+  
+  const handleChange = (e) => {
+    let {name, value} = e.target;
+    setData({
+      ...data,
+      [name]:value
+    })
+  }
+  
   return (
-    <div className="flex justify-center items-center h-6/7">
-        <form className="bg-blue-600 text-white rounded-2xl p-6 flex flex-col w-1/3 items-center justify-center">
+    <div className="flex justify-center items-center h-5/6 sm:h-6/7">
+        <form className="bg-blue-600 text-white rounded-2xl p-6 flex flex-col sm:w-1/3 items-center justify-center">
           <h2 className="text-2xl font-semibold text-center mb-4">Create an Account</h2>
 
           <label htmlFor="name" className="text-xl mb-1">Name</label>
-          <input type="text" id="name" placeholder="Enter your name" className="w-full bg-white text-black mb-3 py-1 px-2 border border-blue-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-950" />
+          <input type="text" id="name" name="name" placeholder="Enter your name" className="w-full bg-white text-black mb-3 py-1 px-2 border border-blue-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-950" value={data.name} onChange={handleChange}/>
 
           <label htmlFor="email" className="text-xl mb-1">Email</label>
-          <input type="email" id="email" placeholder="Enter your emial id" className="w-full bg-white text-black mb-3 py-1 px-2 border border-blue-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-950" />
+          <input type="email" id="email" placeholder="Enter your emial id" className="w-full bg-white text-black mb-3 py-1 px-2 border border-blue-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-950" name="email" value="data.email" onChange={handleChange}/>
 
           <label htmlFor="password" className="text-xl  mb-1">Password</label>
           <input type="password" id="password" placeholder="Create password" className="w-full bg-white text-black mb-3 py-1 px-2 border border-blue-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-950" />
