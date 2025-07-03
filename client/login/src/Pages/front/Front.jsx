abc56c128cb72eefc2efe19ff50f4fc33d9c4145
SadiqRaja1 <sortmode314@gmail.com>
2025-07-03 07:21:29 +0530

    front(dashboard) added

--- client/login/src/Pages/front/Front.jsx
+++ client/login/src/Pages/front/Front.jsx
@@ -1,8 +1,25 @@
 export default function Front () {
+  
   return (
     <>
-      <div>
-        Front
+      <div className="mx-5">
+        <h1 className="text-4xl text-center my-5">Dashboard</h1>
+        <div>
+          <table className="w-full text-xl" >
+            <thead>
+              <tr className="grid grid-cols-3 ">
+                <th className="border text-start px-2 py-1">Name</th>
+                <th className="col-span-2 border text-start px-2 py-1">Email Id</th>
+              </tr>
+            </thead>
+            <tbody className="text-start">
+              <tr className="grid grid-cols-3">
+                <td className="border px-2 py-1">sadiq</td>
+                <td className="col-span-2 border px-2 py-1">sadiq@gmail.com</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </>  
   )
